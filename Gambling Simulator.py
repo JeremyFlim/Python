e04fb00fec3db9569f6b7a75a6b42286234df6a1
derp <99420039+derpAmazing@users.noreply.github.com>
2023-10-18 21:19:08 -0700

    Update Gambling Simulator.py

--- Gambling Simulator.py
+++ Gambling Simulator.py
@@ -1,5 +1,4 @@
 from random import *
-from tkinter import *
 import matplotlib.pyplot as plt
 
 __run__ = True
@@ -18,25 +17,23 @@ def game():
     winChance = winChance/100
 
     gamesDone = 0
-    timesLost = 0
     timesWon = 0
-    steps = 0
 
     coinFlip = False
     money_over_time = []
     win_rate_over_time = []
 
+    steps_for_game = 0
+
     while gamesDone < games:
         stake = money
-        steps_for_game = 0
         while stake > 0 and stake < goal:
             seed(random())
             if random() < winChance:
                 stake = stake + 1
-                steps_for_game += 1
             else:
                 stake = stake - 1
-                steps_for_game += 1
+            steps_for_game += 1
         money_over_time.append(stake)
         if stake >= goal:
             timesWon += 1
@@ -46,8 +43,8 @@ def game():
 
     plt.plot(range(1, games + 1), money_over_time)
     plt.xlabel("Game Number")
-    plt.ylabel("Money")
-    plt.title("Money Over Time")
+    plt.ylabel("Money at end")
+    plt.title("Games won and lost (Not visible at higher game values)")
     plt.show()
 
     plt.plot(range(1, games + 1), win_rate_over_time)
@@ -60,7 +57,7 @@ def game():
     print("\nYou won %s times and lost %s times!" % (timesWon,games - timesWon))
 
     percentageWin = (timesWon/gamesDone)*100
-    stepsPerGame = (steps/games)
+    stepsPerGame = (steps_for_game/games)
 
     print("Your game had a percentage winrate of %s!\n" % percentageWin)
     print("Each game took an average of %s steps to finish!\n" % stepsPerGame)
@@ -77,4 +74,3 @@ def retry():
 
 if __run__ == True:
     game()
-
